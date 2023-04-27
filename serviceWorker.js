61493e91de794a2e0b5ebed88e0586a6d3e5294d
igor <85964276+igcardoso@users.noreply.github.com>
2023-04-27 16:01:11 -0300

    update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,9 @@
 const staticOnuSite = "onu-site-v1";
 const assets = [
   "/",
-  /*"/index.html",
+  "/index.html",
   "/css/app.css",
-  "/css/home.css",
   "/css/global.css",
-  "/css/about_project.css",*/
   "/media/banner.png",
   "/media/banner_2.png",
   "/media/banner_3.png",
