4f29179a851f7c8c363b0c997985e87b2e0363fd
igor <85964276+igcardoso@users.noreply.github.com>
2023-05-09 10:25:40 -0300

    update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,97 @@
+const animated_elements = document.querySelectorAll(".animated_elements");
+
+var windowHeight = window.innerHeight;
+
+function animate_elements() {
+  animated_elements.forEach((animated_element) => {
+    let bounding = animated_element.getBoundingClientRect();
+    if (bounding.bottom > windowHeight + 200) {
+      animated_element.classList.add("is-down");
+    } else if (bounding.top < 0) {
+      animated_element.classList.add("is-up");
+    } else {
+      animated_element.classList.remove("is-up");
+      animated_element.classList.remove("is-down");
+    }
+  });
+}
+
+document.addEventListener("scroll", function () {
+  animate_elements();
+  document.removeEventListener("scroll", this);
+});
+
+window.addEventListener("resize", function () {
+  windowHeight = window.innerHeight;
+  window.removeEventListener("resize", this);
+});
+
+animate_elements();
+
+/*========= animate stars =========*/
+
+const stars_elements = document.querySelectorAll(".stars");
+
+function animate_stars() {
+  stars_elements.forEach((star_element) => {
+    let bounding = star_element.getBoundingClientRect();
+    if (bounding.bottom > windowHeight) {
+      star_element.classList.add("stars-down");
+      star_element.classList.remove("stars-up");
+    } else if (bounding.bottom < windowHeight) {
+      star_element.classList.add("stars-up");
+      star_element.classList.remove("stars-down");
+    } else {
+      star_element.classList.remove("stars-up");
+      star_element.classList.remove("stars-down");
+    }
+  });
+}
+
+document.addEventListener("scroll", function () {
+  animate_stars();
+  document.removeEventListener("scroll", this);
+});
+
+window.addEventListener("resize", function () {
+  windowHeight = window.innerHeight;
+  window.removeEventListener("resize", this);
+});
+
+animate_stars();
+
+/*========= animate statistics =========*/
+
+const statistics_elements = document.querySelectorAll(".statistic");
+
+function animate_statistics() {
+  statistics_elements.forEach((statistic_element) => {
+    let bounding = statistic_element.getBoundingClientRect();
+    if (bounding.bottom > windowHeight) {
+      statistic_element.classList.add("statistics-down");
+      statistic_element.classList.remove("statistics-up");
+    } else if (bounding.bottom < windowHeight) {
+      statistic_element.classList.add("statistics-up");
+      statistic_element.classList.remove("statistics-down");
+    } else {
+      statistic_element.classList.remove("statistics-up");
+      statistic_element.classList.remove("statistics-down");
+    }
+  });
+}
+
+document.addEventListener("scroll", function () {
+  animate_statistics();
+  document.removeEventListener("scroll", this);
+});
+
+window.addEventListener("resize", function () {
+  windowHeight = window.innerHeight;
+  window.removeEventListener("resize", this);
+});
+
+animate_statistics();
+
 var nav_bar = document.querySelector("header");
 
 window.addEventListener("scroll", ()=> {
@@ -43,6 +137,10 @@ back.addEventListener("click", ()=> {
 });
 
 
+if (position < 0) {
+  position = 4;
+}
+
 function current_position() {
 
   switch (position) {
@@ -76,6 +174,64 @@ function current_position() {
       document.querySelector(".active").classList.remove('active');
       document.querySelector(".thumbs1").classList.add('active');
 
-      position = 0;
+      position = 1;
     }
   }
+
+  /*========= first slide =========*/
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var stream = document.querySelector('.gallery__stream.s1');
+    var items = document.querySelectorAll('.gallery__item.s1');
+
+    var prev = document.querySelector('.gallery__prev.s1');
+    prev.addEventListener('click', function() {
+      stream.insertBefore(items[items.length - 1], items[0]);
+      items = document.querySelectorAll('.gallery__item.s1');
+    });
+
+    var next = document.querySelector('.gallery__next.s1');
+    next.addEventListener('click', function() {
+      stream.appendChild(items[0]);
+      items = document.querySelectorAll('.gallery__item.s1');
+    });
+  });
+
+
+  /*========= second slide =========*/
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var stream = document.querySelector('.gallery__stream.s2');
+    var items = document.querySelectorAll('.gallery__item.s2');
+
+    var prev = document.querySelector('.gallery__prev.s2');
+    prev.addEventListener('click', function() {
+      stream.insertBefore(items[items.length - 1], items[0]);
+      items = document.querySelectorAll('.gallery__item.s2');
+    });
+
+    var next = document.querySelector('.gallery__next.s2');
+    next.addEventListener('click', function() {
+      stream.appendChild(items[0]);
+      items = document.querySelectorAll('.gallery__item.s2');
+    });
+  });
+
+  /*========= second slide =========*/
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var stream = document.querySelector('.gallery__stream.s3');
+    var items = document.querySelectorAll('.gallery__item.s3');
+
+    var prev = document.querySelector('.gallery__prev.s3');
+    prev.addEventListener('click', function() {
+      stream.insertBefore(items[items.length - 1], items[0]);
+      items = document.querySelectorAll('.gallery__item.s3');
+    });
+
+    var next = document.querySelector('.gallery__next.s3');
+    next.addEventListener('click', function() {
+      stream.appendChild(items[0]);
+      items = document.querySelectorAll('.gallery__item.s3');
+    });
+  });
